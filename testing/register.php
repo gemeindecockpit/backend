59ad4f5e0e5ccc9e3d24232bc60795476c59938d
Alx <litwinow@gmx.net>
2020-08-08 02:13:23 +0200

    -added comments to index.php -added register.php with some logic

---
+++ testing/register.php
@@ -0,0 +1,41 @@
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 well">
+			<form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="signupform">
+				<fieldset>
+					<legend>Sign Up</legend>
+					<div class="form-group">
+						<label for="name">Name</label>
+						<input type="text" name="name" placeholder="Enter Full Name" required value="<?php if($error) echo $name; ?>" class="form-control" />
+						<span class="text-danger"><?php if (isset($uname_error)) echo $uname_error; ?></span>
+					</div>					
+					<div class="form-group">
+						<label for="name">Email</label>
+						<input type="text" name="email" placeholder="Email" required value="<?php if($error) echo $email; ?>" class="form-control" />
+						<span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
+					</div>
+					<div class="form-group">
+						<label for="name">Password</label>
+						<input type="password" name="password" placeholder="Password" required class="form-control" />
+						<span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
+					</div>
+					<div class="form-group">
+						<label for="name">Confirm Password</label>
+						<input type="password" name="cpassword" placeholder="Confirm Password" required class="form-control" />
+						<span class="text-danger"><?php if (isset($cpassword_error)) echo $cpassword_error; ?></span>
+					</div>
+					<div class="form-group">
+						<input type="submit" name="signup" value="Sign Up" class="btn btn-primary" />
+					</div>
+				</fieldset>
+			</form>
+			<span class="text-success"><?php if (isset($success_message)) { echo $success_message; } ?></span>
+			<span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 text-center">	
+		Already Registered? <a href="login.php">Login Here</a>
+		</div>
+	</div>	
+</div>
