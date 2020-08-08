ccc57871d251adbdfaff1f7c653f376dc2791044
Alx <litwinow@gmx.net>
2020-08-08 02:38:00 +0200

    -added registering functionality (should work this time lmao)

--- testing/register.php
+++ testing/register.php
@@ -39,3 +39,44 @@
 		</div>
 	</div>	
 </div>
+<?php
+//this section will be executed apon receiving a post request from the submit form
+required("db_connect.php");
+session_start();
+if(isset($_SESSION['user_id'])) {
+	header("Location: testing/index.php");
+}
+$error = false;
+if (isset($_POST['signup'])) {
+	//mysqli_real_escape_string: Escapes special characters in a string for use in an SQL statement, taking into account the current charset of the connection
+	//so all the fields submited by the form are taken care of to not be abused to execute unintentional sql queries
+	$name = mysqli_real_escape_string($conn, $_POST['name']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	$cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);	
+	//some regex validation
+	if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+		$error = true;
+		$uname_error = "Name must contain only alphabets and space";
+	}
+	if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+		$error = true;
+		$email_error = "Please Enter Valid Email ID";
+	}
+	if(strlen($password) < 6) {
+		$error = true;
+		$password_error = "Password must be minimum of 6 characters";
+	}
+	if($password != $cpassword) {
+		$error = true;
+		$cpassword_error = "Password and Confirm Password doesn't match";
+	}
+	if (!$error) {
+		if(mysqli_query($conn, "INSERT INTO users(user, email, pass) VALUES('" . $name . "', '" . $email . "', '" . md5($password) . "')")) {
+			$success_message = "Successfully Registered! <a href='testing/login.php'>Click here to Login</a>";
+		} else {
+			$error_message = "Error in registering...Please try again later!";
+		}
+	}
+}
+?>
