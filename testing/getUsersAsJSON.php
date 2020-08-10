a27f3ab015484a26258911eaf3423e622bd4f241
Alx <litwinow@gmx.net>
2020-08-10 21:28:32 +0200

    - testing sending db entries as json

--- testing/getUsersAsJSON.php
+++ testing/getUsersAsJSON.php
@@ -5,9 +5,13 @@ if(isset($_SESSION['user_id'])) {
 	foreach ( $conn->query('SELECT * FROM users') as $row ) {
 		print_r($row);//echo "{$row['field']}";
 	}
-	$result = mysqli_query($conn, "SELECT * FROM users");
-	$myJson = json_encode($result);
-	echo $myJson;
+	//mysqli_fetch_assoc — Fetch a result row as an associative array
+	$sth = mysqli_query($conn, "SELECT * FROM users");
+	$rows = array();
+	while($r = mysqli_fetch_assoc($sth)) {
+		$rows[] = $r;
+	}
+	print json_encode($rows);
 } else {
 	printf("Sorry you are not logged in.");
 }
