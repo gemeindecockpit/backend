a837b3312ea1619773896b2536e7e486474ad543
Alx <litwinow@gmx.net>
2020-08-17 22:19:19 +0200

    testing

--- index.php
+++ index.php
@@ -82,6 +82,10 @@ else
 		}
 		else
 		{
+				$result = $data_operation->get_user('test');
+				if ($row = mysqli_fetch_array($result)) {
+					echo $row['email'];
+				}
 			//Debug: create a user to test the login and register function
 			echo '<br><span> your are logged in but a User with the username : testus and the pw: testtest has been created for you</span>' . $user->register('testus', 'testtest', 'test@email.com', 'realname', 'wiesoisthiereinsaltinderdatenbanklmao');	
 		}
