456b075ddd40d36e9eb903eabb90d100f3be34ec
Djentleman2414 <t.galonska@ostfalia.de>
2020-08-19 00:33:53 +0200

    organized the dummy-api a bit

--- inc/config.php
+++ inc/config.php
@@ -16,7 +16,20 @@ define('NUTS0', 'deutschland');
 define('NUTS1', 'niedersachsen');
 define('NUTS2', 'braunschweig');
 define('NUTS3', 'braunschweig');
-define('DOMAIN', 'litwinow.xyz');
+define('DOMAIN', 'litwinow.xyz/');
+
+define('URI_CONFIG', DOMAIN . 'config/');
+
+define('URI_WF_PLZ', URI_CONFIG . '38300/');
+
+define('URI_WF_FEUERWEHREN', URI_WF_PLZ . 'feuerwehr/');
+
+define('URI_WF_FEUERWEHR', URI_WF_FEUERWEHREN . 'WF%20Feuerwehr/');
+define('URI_FREIWILLIGE_FEUERWEHR', URI_WF_FEUERWEHREN . 'Freiwillige%20Feuerwehr/');
+
+define('URI_EINSATZKRAEFTE', URI_WF_FEUERWEHR . 'einsatkraefte/');
+define('URI_FAHRZEUGE', URI_WF_FEUERWEHR . 'fahrzeuge/');
+
 
 
 
@@ -26,65 +39,144 @@ define('SALT', 'wevsvirus');
 
 # more defines here
 //Debugging
-define('DUMMY_FEUERWEHR', array('id' => 1,
-	 'name' => 'feuerwerk',
+
+/*
+Template for JSON define:
+
+define('NAME', array(
+		'id' =>,
+		'links' => array(
+			'self' =>,
+			'next' => array(
+
+			)
+		)
+	)
+);
+
+*/
+
+//All organisations
+define('ALLEN_ORGANISATION', array(
+  'links' => array(
+      'self' => URI_CONFIG,
+      '38300' => array(
+        'feuerwehr' => URI_WF_FEUERWEHREN
+      )
+    )
+  )
+);
+
+// All Organisations in a PLZ
+define('PLZ_38300', array(
+		'links' => array(
+			'self' => URI_WF_PLZ,
+			'next' => array(
+				'feuerwehr' => URI_WF_FEUERWEHREN
+			)
+		)
+	)
+);
+
+
+// All Organisations of a specific type (in a certain plz)
+define('WF_FEUERWEHREN', array(
+		'links' => array(
+			'self' => URI_WF_FEUERWEHREN,
+			'next' => array(
+				'WF Feuerwehr' => URI_WF_FEUERWEHR,
+				'Freiwillige Feuerwehr' => URI_FREIWILLIGE_FEUERWEHR
+			)
+		)
+	)
+);
+
+
+// confic of the specific organisations
+define('WF_FEUERWEHR', array(
+	 'id' => 1,
+	 'name' => 'WF Feuerwehr',
 	 'description' => 'testetst',
 	 'type' => 'feuerwehr',
 	 'contact' => 'feuer@wehr.com',
-	 'links' => array('self' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/',
-				'fields' => array('Einsatzkraefte' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/einsatzkraefte/',
-								'Autos broom broom' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/autos%20broom%20broom'))));
+	 'links' => array(
+		 'self' => URI_WF_FEUERWEHR,
+			'fields' => array(
+				'Einsatzkraefte' => URI_EINSATZKRAEFTE,
+				'Fahrzeuge' => URI_FAHRZEUGE
+			)
+		)
+	)
+);
 
-define('DUMMY_FEUERWEHR2', array('id' => 2,
-	 'name' => 'Wir machen Nass',
+define('FREIWILLIGE_FEUERWEHR', array(
+	 'id' => 2,
+	 'name' => 'Freiwillige Feuerwehr',
 	 'description' => 'heh',
 	 'type' => 'feuerwehr',
 	 'contact' => 'feuer@nass.com',
-	 'links' => array('self' => 'litwinow.xyz/config/38000/feuerwehr/wir%20machen%20nass/')));
+	 'links' => array(
+		 'self' => URI_FREIWILLIGE_FEUERWEHR
+ 		)
+	)
+);
 
-define('DUMMY_FEUERWEHR_FELD1', array('id' => 1,
+
+// config of the fields
+define('WF_FEUERWEHR_FELD1', array(
+	 'id' => 1,
 	 'name' => 'Einsatzkraefte',
 	 'max_value' => 5.0,
 	 'yellow_value' => 4.0,
 	 'red_value' => 3.0,
 	 'relational_flag' => true,
-	 'links' => array('self' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/einsatzkraefte/')));
-
- define('DUMMY_FEUERWEHR_FELD2', array('id' => 2,
-		 'name' => 'Autos broom broom',
-		 'max_value' => 'null',
-		 'yellow_value' => 4.0,
-		 'red_value' => 3.0,
-		 'relational_flag' => false,
-		 'links' => array('self' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/autos%20broom%20broom/')));
-
-define('EINSATZKRAEFTE_2020_08_15', array('field_id' => 1,
-		 'field_name' => 'einsatkraefte',
-		 'user_id' => '1',
-		 'username' => 'testus',
-		 'value' => '3',
-		 'date' => '11-12-2020',
-		 'links' => array('self' => 'litwinow.xyz/data/38000/feuerwehr/feuerwerk/einsatzkraefte/2020/12/11/')));
-
-define('EINSATZKRAEFTE_2020_08_16', array('field_id' => 1,
-		 'field_name' => 'einsatkraefte',
-		 'user_id' => '1',
-		 'username' => 'testus',
-		 'value' => '3',
-		 'date' => '12-12-2020',
-		 'links' => array('self' => 'litwinow.xyz/data/38000/feuerwehr/feuerwerk/einsatzkraefte/2020/12/12/')));
-
-define('ALLEN_ORGANISATION', array(
-  'links' => array(
-      'self' => 'litwinow.xyz/config/',
-      '38300' => array(
-        'feuerwehr' => array(
-          'feuerwerk' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/',
-          'wir machen nass' => 'litwinow.xyz/config/38000/feuerwehr/wir%20machen%20nass/'
-        )
-      )
-    )
-  )
+	 'links' => array(
+		 'self' => URI_EINSATZKRAEFTE
+ 		)
+	)
 );
+
+ define('WF_FEUERWEHR_FELD2', array(
+   'id' => 2,
+	 'name' => 'Autos broom broom',
+	 'max_value' => 'null',
+	 'yellow_value' => 4.0,
+	 'red_value' => 3.0,
+	 'relational_flag' => false,
+	 'links' => array(
+		 'self' => URI_FAHRZEUGE
+ 		)
+ 	)
+);
+
+
+// the actual data
+define('EINSATZKRAEFTE_2020_08_15', array(
+	 'field_id' => 1,
+	 'field_name' => 'einsatkraefte',
+	 'user_id' => '1',
+	 'username' => 'testus',
+	 'value' => '3',
+	 'date' => '11-12-2020',
+	 'links' => array(
+		 'self' => URI_EINSATZKRAEFTE . '2020/08/15/'
+		)
+ 	)
+);
+
+define('EINSATZKRAEFTE_2020_08_16', array(
+	 'field_id' => 1,
+	 'field_name' => 'einsatkraefte',
+	 'user_id' => '1',
+	 'username' => 'testus',
+	 'value' => '3',
+	 'date' => '12-12-2020',
+	 'links' => array(
+		 'self' => URI_EINSATZKRAEFTE . '2020/08/16/'
+	 )
+ )
+);
+
+
 
 ?>
--- index.php
+++ index.php
@@ -39,52 +39,14 @@ if(isset($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'logout')
 }
 */
 if(isset($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'config') {
-	if(isset($uri_info->path_vars[1]) && $uri_info->path_vars[1] == '38000' &&
-			isset($uri_info->path_vars[2]) && $uri_info->path_vars[2] == 'feuerwehr') {
-
-				if(isset($uri_info->path_vars[3]) && $uri_info->path_vars[3] == 'feuerwerk') {
-					if(isset($uri_info->path_vars[4]) && $uri_info->path_vars[4] == 'einsatzkraefte') {
-						if(isset($uri_info->path_vars[7]) && $uri_info->path_vars[7]  = '11') {
-							header('Content-type: application/json');
-							$temp = new DataOutput();
-							echo $temp->output_as_json(EINSATZKRAEFTE_2020_08_15);
-						} else if (isset($uri_info->path_vars[7]) && $uri_info->path_vars[7]  = '12') {
-							header('Content-type: application/json');
-							$temp = new DataOutput();
-							echo $temp->output_as_json(EINSATZKRAEFTE_2020_08_16);
-						} else {
-							header('Content-type: application/json');
-							$temp = new DataOutput();
-							echo $temp->output_as_json(DUMMY_FEUERWEHR_FELD1);
-						}
-					} else if (isset($uri_info->path_vars[4]) && $uri_info->path_vars[4] == 'autos%20broom%20broom') {
-						header('Content-type: application/json');
-						$temp = new DataOutput();
-						echo $temp->output_as_json(DUMMY_FEUERWEHR_FELD2);
-					} else {
-						header('Content-type: application/json');
-						$temp = new DataOutput();
-						echo $temp->output_as_json(DUMMY_FEUERWEHR);
-					}
-				} else if (isset($uri_info->path_vars[3]) && $uri_info->path_vars[3] == 'wir%20machen%20nass'){
-					header('Content-type: application/json');
-					$temp = new DataOutput();
-					echo $temp->output_as_json(DUMMY_FEUERWEHR2);
-				} else {
-					echo 'Computer sagt nein';
-				}
-
-	} else {
-		header('Content-type: application/json');
-		$temp = new DataOutput();
-		echo $temp->output_as_json(ALLEN_ORGANISATION);
-	}
+	config_call($uri_info,$data_out);
+} else if($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'data') {
+	data_call($uri_info,$data_out);
 }	else {
 
 	if(isset($_SESSION['userid']))
 	{
 		echo '<br><span> your are logged in</span>';
-
 	}
 
 	else
@@ -104,6 +66,54 @@ if(isset($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'config') {
 			echo '<br><span> you are not logged in but a User with the username : testus and the pw: testtest has been created for you</span>' . $user->register('testus', 'testtest', 'test@email.com', 'realname', 'wiesoisthiereinsalt');
 		}
 	}
+}
+
+function config_call($uri_info, $data_out) {
+	if(isset($uri_info->path_vars[1]) && $uri_info->path_vars[1] == '38000') {
+			if(isset($uri_info->path_vars[2]) && $uri_info->path_vars[2] == 'feuerwehr'){
+					if(isset($uri_info->path_vars[3]) && $uri_info->path_vars[3] == 'WF%20Feuerwehr') {
+							if(isset($uri_info->path_vars[4]) && $uri_info->path_vars[4] == 'einsatzkraefte') {
+								header('Content-type: application/json');
+								echo $data_out->output_as_json(WF_FEUERWEHR_FELD1);
+							} else if (isset($uri_info->path_vars[4]) && $uri_info->path_vars[4] == 'fahrzeuge') {
+								header('Content-type: application/json');
+								echo $data_out->output_as_json(WF_FEUERWEHR_FELD2);
+							} else if (isset($uri_info->path_vars[4])) {
+								echo "Andere Felder gibt es noch nicht";
+							} else {
+								header('Content-type: application/json');
+								echo $data_out->output_as_json(WF_FEUERWEHR);
+							}
+					} else if (isset($uri_info->path_vars[3]) && $uri_info->path_vars[3] == 'Freiwillige%20Feuerwehr') {
+						if (isset($uri_info->path_vars[4])) {
+							echo 'Für die freiwillige Feuerwehr wurden noch keine Felder angelegt';
+						} else {
+							header('Content-type: application/json');
+							echo $data_out->output_as_json(FREIWILLIGE_FEUERWEHR);
+						}
+					} else if(isset($uri_info->path_vars[3])) {
+						echo "Diese Feuerwehr gibt es nicht";
+					} else {
+						header('Content-type: application/json');
+						echo $data_out->output_as_json(WF_FEUERWEHREN);
+					}
+			} else if(isset($uri_info->path_vars[2])) {
+				echo "Bisher wurden nur Feuerwehren definiert";
+			} else {
+				header('Content-type: application/json');
+				echo $data_out->output_as_json(PLZ_38300);
+			}
+	} else if(isset($uri_info->path_vars[1])) {
+		echo 'Bisher gibt es nur Kacheln für Wolfenbüttel';
+	} else {
+		header('Content-type: application/json');
+		echo $data_out->output_as_json(ALLEN_ORGANISATION);
+	}
+	return;
+}
+
+function data_call($uri_info,$data_out){
+	return;
 }
 
 
