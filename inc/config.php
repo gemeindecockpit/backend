c2393e52687fcdb19dd04a6bc1a01811cac601f6
Alx <litwinow@gmx.net>
2020-08-18 16:06:32 +0200

    added dummy file for config - organization

--- inc/config.php
+++ inc/config.php
@@ -16,6 +16,8 @@ define('NUTS0', 'deutschland');
 define('NUTS1', 'niedersachsen');
 define('NUTS2', 'braunschweig');
 define('NUTS3', 'braunschweig');
+define('DOMAIN', 'litwinow.xyz');
+
 
 
 
@@ -24,19 +26,28 @@ define('SALT', 'wevsvirus');
 
 # more defines here
 //Debugging
-define('DUMMY_FEUERWEHR', array('id' => 1, 'name' => 'feuerwerk', 'description' => 'testetst', 'type' => 'feuerwehr', 'contact' => 'feuer@wehr.com'));
+define('DUMMY_FEUERWEHR', array('id' => 1,
+	 'name' => 'feuerwerk',
+	 'description' => 'testetst',
+	 'type' => 'feuerwehr',
+	 'contact' => 'feuer@wehr.com'
+	 'links' => array('self' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/',
+				'fields' => array('Einsatzkraefte' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/einsatzkraefte/',
+								'Autos broom broom' => 'litwinow.xyz/config/38000/feuerwehr/feuerwerk/autos%20broom%20broom'))));
+
 define('DUMMY_FEUERWEHR_FELD1', array('id' => 1,
- 'name' => 'Einsatzkräfte',
- 'max_value' => 5.0,
- 'yellow_value' => 4.0,
- 'red_value' => 3.0,
- 'relational_flag' => true));
+	 'name' => 'Einsatzkraefte',
+	 'max_value' => 5.0,
+	 'yellow_value' => 4.0,
+	 'red_value' => 3.0,
+	 'relational_flag' => true));
+ 
  define('DUMMY_FEUERWEHR_FELD2', array('id' => 2,
- 'name' => 'Autos broom broom',
- 'max_value' => 'null',
- 'yellow_value' => 4.0,
- 'red_value' => 3.0,
- 'relational_flag' => false));
+	 'name' => 'Autos broom broom',
+	 'max_value' => 'null',
+	 'yellow_value' => 4.0,
+	 'red_value' => 3.0,
+	 'relational_flag' => false));
 ?>
 
 
--- index.php
+++ index.php
@@ -53,6 +53,18 @@ if(isset($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'config' &&
 } else if(isset($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'config' && 
 	isset($uri_info->path_vars[1]) && $uri_info->path_vars[1] == '38000' && 
 	isset($uri_info->path_vars[2]) && $uri_info->path_vars[2] == 'feuerwehr' &&
+	isset($uri_info->path_vars[3]) && $uri_info->path_vars[3] == 'feuerwerk' &&
+	isset($uri_info->path_vars[4]) && $uri_info->path_vars[4] == 'auto%20broom%20broom') 
+{
+	header('Content-type: application/json');
+	$temp = new DataOutput();
+	echo $temp->output_as_json(DUMMY_FEUERWEHR_FELD2);
+	
+	
+	
+} else if(isset($uri_info->path_vars[0]) && $uri_info->path_vars[0] == 'config' && 
+	isset($uri_info->path_vars[1]) && $uri_info->path_vars[1] == '38000' && 
+	isset($uri_info->path_vars[2]) && $uri_info->path_vars[2] == 'feuerwehr' &&
 	isset($uri_info->path_vars[3]) && $uri_info->path_vars[3] == 'feuerwerk') {
 	header('Content-type: application/json');
 	$temp = new DataOutput();
