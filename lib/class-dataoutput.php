48fc257b6f8b4d79b83c0a0fb550a904e45bf7e4
Alx <litwinow@gmx.net>
2020-08-21 04:24:14 +0200

    added a function to get the current url and added a links array that will automaticly be added to the output array and added the self link to all links

--- lib/class-dataoutput.php
+++ lib/class-dataoutput.php
@@ -6,24 +6,28 @@
 # like json etc.
 
 class DataOutput {
-
+	$links_array = array();
+	
 	public function __construct() 
 	{
+		add_keyvalue_to_links_array('self', get_current_self_link());
         return;
     }
 
 
 	public function output_as_json($data_array)
 	{
+		$data_array['links'] = $links_array;
 		$json_string = json_encode($data_array);
-
 		return $json_string;
 	}
 	//adds a field to the data that will be converted to json. Mainly used for HATEOAS-Links
-	public function add_field_to_output_array($field){
-		
+	public function add_keyvalue_to_links_array($key, $value){
+		$links_array[$key] = $value;
 	}
 	//returns a link to the current resource
+	//TODO look into alternatives for http_host and request_uri. The client can set HTTP_HOST and REQUEST_URI to any arbitrary value it wants.
+	//TODO check if params are also shown
 	public function get_current_self_link(){
 		return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 	}
