e66be7a75c13a8f1129f3e66b7ddd8cb91bb65b9
Alx <litwinow@gmx.net>
2020-09-11 22:13:58 +0200

    added cookie encryption

--- app/settings.php
+++ app/settings.php
@@ -10,6 +10,7 @@ return function (ContainerBuilder $containerBuilder) {
         'settings' => [
             'displayErrorDetails' => true, // Should be set to false in production
             'determineRouteBeforeAppMiddleware' => true, //Without this, you will not be able to access the name of the route from within the middleware and will retrieve a null object.
+            'cookies.encrypt' => true,
             'logger' => [
                 'name' => 'slim-app',
                 'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
