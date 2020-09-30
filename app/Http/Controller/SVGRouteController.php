5e91a21121569d881b25a78d1708dc4640384382
Alx <litwinow@gmx.net>
2020-10-01 01:11:36 +0200

    changed random rename behaviour

--- app/Http/Controller/SVGRouteController.php
+++ app/Http/Controller/SVGRouteController.php
@@ -90,7 +90,7 @@ class SVGRouteController extends RouteController {
         $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
 
         // see http://php.net/manual/en/function.random-bytes.php
-        $basename = bin2hex(random_bytes(8));
+        $basename = bin2hex(random_bytes(8)) . $uploadedFile->getClientFilename();
         $filename = sprintf('%s.%0.8s', $basename, $extension);
 
         $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
